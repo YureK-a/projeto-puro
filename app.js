b2efabc18b9df6e75d8441caa7173aa972da5a74
YureK-a <yurealgumacoisa@gmail.com>
2023-07-18 07:49:30 -0300

    modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const mainPage = document.querySelector('[data-js="main-page"]')
 const actualScreen = document.querySelector('[data-screen="initial"]')
 const ifrnLogo = document.querySelector('[data-logo="ifrn-logo"]')
 
-const loremIpsum = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-const loremIpsum2 = `Clique aqui para obter o regimento interno do NAPNE`
-const loremIpsum3 = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, comes from a line in section 1.10.32."
-
 ifrnLogo.addEventListener('click', () => {
   const initialScreenNotExists = actualScreen.dataset.screen !== 'initial'
 
