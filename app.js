a95807788ff7fa0e31231989719c0f1e93c99003
King-Sinbad <131402046+King-Sinbad@users.noreply.github.com>
2023-07-20 18:49:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -106,9 +106,95 @@ nav.addEventListener('click', e => {
     p.setAttribute('class', 'p-text')
     h1.setAttribute('class', 'proposta-screen')
 
-    h1.textContent = 'Contato'
-    p.textContent = 'insira aqui os contatos'
+   
     actualScreen.dataset.screen = 'contato'
+    
+    div.innerHTML += `
+  <div class="margin"><div class="fonttt">Contatos</div></div>
+  <div class="fontp"><p> ⠀⠀⠀⠀Para entrar em contato conosco basta ligar para o número de qualquer um dos profissionais do Napne. Os seus contatos estão disponivéis logo abaixo.</p></div>
+  <div class="margin">
+  <table id="customers">
+  <tr>
+    <th>Nome</th>
+    <th>Função</th>
+    <th>Contato</th>
+  </tr>
+  <tr>
+    <td>Marclzéia Melo de Souza Queiro</td>
+    <td>Coordenadora/Pedagoga</td>
+    <td>(84) 98808 8182</td>
+  </tr>
+  <tr>
+    <td>Maria Alexandra Ribeiro Pinto</td>
+    <td>Representante docente</td>
+    <td>(84) 99952 2857</td>
+  </tr>
+  <tr>
+    <td>Anna Nery Dantas Camacho Varela</td>
+    <td>Representante docente</td>
+    <td>(84) 99411 5687</td>
+  </tr>
+  <tr>
+    <td>Glaudénia Alves De Moura</td>
+    <td>Psicóloga</td>
+    <td>(84) 99945 7326</td>
+  </tr>
+  <tr>
+    <td>Magnólia Maria da Rocha Melo</td>
+    <td>Assistente Social</td>
+    <td>(84) 98825 1417</td>
+  </tr>
+  <tr>
+    <td>Suely Katiana Lima Costa Guerra</td>
+    <td>Psicopedagoga</td>
+    <td>(84) 99198 0725</td>
+  </tr>
+  <tr>
+    <td>Maria Leania Mendes</td>
+    <td>Psicopedagoga</td>
+    <td>(84) 98795 2587</td>
+  </tr>
+  <tr>
+    <td>Karine Menezes Ribeiro</td>
+    <td>Assistente Educacional Inclusivo</td>
+    <td>(84) 99625 9291</td>
+  </tr>
+  <tr>
+    <td>Vanuzia Maria De Medeiros</td>
+    <td>Ledora</td>
+    <td>(84) 99943 8367</td>
+  </tr>
+  <tr>
+    <td>Bartira Raranaya Ge Pontess</td>
+    <td>Cuidadora</td>
+    <td>(84) 98719 2759</td>
+  </tr>
+  <tr>
+    <td>Alexandre Fagner Garcia Medeiros Rocha</td>
+    <td>Intérprete de Lingua de Sinais</td>
+    <td>(84) 98200 8568</td>
+  </tr>
+  <tr>
+    <td> Maria Sara Izidio Da Silva</td>
+    <td>Intérprete de Lingua de Sinais</td>
+    <td>(84) 98856 6589</td>
+  </tr>
+  <tr>
+    <td>Elisberta De Oliveira Araujo</td>
+    <td>Intérprete de Lingua de Sinais</td>
+    <td>(84) 99947 1341</td>
+  </tr>
+  </table>
+  </div>
+  <br>
+  <div class="fontp">
+  <table><thead><tr>Também conheça nosso instagram:</tr>
+  <tr><a href="https://www.instagram.com/napne.mo/?igshid=MmU2YjMzNjRlOQ%3D%3D"><img src='./imgs/insta.png' width="50"></img></a> </tr>
+  </thead>
+  </table>
+  </div>
+  `
+
 
     div.append(h1, p)
     actualScreen.append(div)
